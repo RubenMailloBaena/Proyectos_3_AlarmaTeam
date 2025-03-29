d05f2c06d0c85fbf8eddd36bd71bf9f140d5aeaa
RubenMailloBaena <rubenmaillo2003@gmail.com>
2025-03-29 12:20:17 +0100

    starting point

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,21 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private InputActionReference moveInput;
+    
+    [Header("Movement Attributes")] 
+    [SerializeField] private float playerSpeed = 5f;
+
+    private CharacterController charController;
+    private Vector3 direction;
+    
     private void Start()
     {
-        throw new NotImplementedException();
+        charController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
+        ProcessInput();
         Move();
+    }
+
+    private void ProcessInput()
+    {
+        Vector2 input = moveInput.action.ReadValue<Vector2>();
+        direction = new Vector3(input.x, 0f, input.y).normalized;
     }
 
     private void Move()
     {
-        
+        charController.Move(direction * (playerSpeed * Time.deltaTime));
     }
 }
