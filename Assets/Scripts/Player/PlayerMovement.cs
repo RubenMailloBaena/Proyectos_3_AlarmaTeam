f3ca72ace4c830b9a687ad2e9a2e4dd7c470cc24
RubenMailloBaena <rubenmaillo2003@gmail.com>
2025-03-29 11:30:11 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        
+    }
+}
