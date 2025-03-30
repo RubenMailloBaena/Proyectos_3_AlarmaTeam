33446f2d2cbbade291854a8b8b96f8f2c63b79e0
RubenMailloBaena <rubenmaillo2003@gmail.com>
2025-03-31 01:21:59 +0200

    Lean Improved

--- Assets/Scripts/Player/PlayerLeanController.cs
+++ Assets/Scripts/Player/PlayerLeanController.cs
@@ -1,58 +1,70 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerLeanController : MonoBehaviour
 {
     [SerializeField] private InputActionReference leanInput;
-    [SerializeField] private Animator animator;
     [SerializeField] private Transform pitchController;
-    
-    private RaycastHit hit;
+    [SerializeField] private Transform leanParent;
+
+    [Header("Lean Attributes")]
+    [SerializeField] private float moveAmount = 0.45f;
+    [SerializeField] private float leanAngle = 20f;
+    [SerializeField] private float leanSpeed = 5f;
+
     private float input;
     private PlayerMovementController playerMovementController;
+    private Vector3 originalPosition, targetPosition;
+    private Quaternion originalRotation, targetRotation;
 
     private void Awake()
     {
         playerMovementController = GetComponent<PlayerMovementController>();
     }
 
+    private void Start()
+    {
+        originalPosition = leanParent.localPosition;
+        originalRotation = leanParent.localRotation;
+    }
+
     private void Update()
     {
+        SetLeanTarget();
         LeanPlayer();
     }
 
-    private void LeanPlayer()
+    private void SetLeanTarget()
     {
         if (!playerMovementController.IsGrounded()) return;
-        
+
         input = leanInput.action.ReadValue<float>();
         
+        //IDLE
+        targetPosition = originalPosition;
+        targetRotation = originalRotation;
+        playerMovementController.SetIsLeaning(false);
+
         Vector3 pos = new Vector3(transform.position.x, pitchController.position.y, transform.position.z);
-            
-        if (input < 0 && !Physics.Raycast(pos, -transform.right, out hit, 1f))
+        
+        if (input < 0 && !Physics.Raycast(pos, -transform.right, 1f)) //LEFT
         {
-            animator.ResetTrigger("Idle");
-            animator.ResetTrigger("Right");
-            animator.SetTrigger("Left");
+            targetPosition += Vector3.left * moveAmount; 
+            targetRotation *= Quaternion.Euler(0, 0, leanAngle); 
             playerMovementController.SetIsLeaning(true);
         }
-        else if (input > 0 && !Physics.Raycast(pos, transform.right, out hit, 1f))
+        else if (input > 0  && !Physics.Raycast(pos, transform.right, 1f)) //RIGHT
         {
-            animator.ResetTrigger("Idle");
-            animator.ResetTrigger("Left");
-            animator.SetTrigger("Right");
+            targetPosition += Vector3.right * moveAmount; 
+            targetRotation *= Quaternion.Euler(0, 0, -leanAngle); 
             playerMovementController.SetIsLeaning(true);
         }
-        else
-        {
-            animator.ResetTrigger("Right");
-            animator.ResetTrigger("Left");
-            animator.SetTrigger("Idle");
-            playerMovementController.SetIsLeaning(false);
-        }
+    }
+    
+    private void LeanPlayer()
+    {
+        leanParent.localPosition = Vector3.Lerp(leanParent.localPosition, targetPosition, Time.deltaTime * leanSpeed);
+        leanParent.localRotation = Quaternion.Slerp(leanParent.localRotation, targetRotation, Time.deltaTime * leanSpeed);
     }
 }
