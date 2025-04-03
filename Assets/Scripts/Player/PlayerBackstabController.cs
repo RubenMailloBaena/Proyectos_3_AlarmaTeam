3f8375d52d76178ca7ca487cf8dd7842c6d74477
RubenMailloBaena <121771440+RubenMailloBaena@users.noreply.github.com>
2025-04-03 18:33:47 +0200

    BackStabCOntroller

--- Assets/Scripts/Player/PlayerBackstabController.cs
+++ Assets/Scripts/Player/PlayerBackstabController.cs
@@ -4,39 +4,54 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerController))]
-public class BackStabController : MonoBehaviour
+public class PlayerBackstabController : MonoBehaviour
 {
     [SerializeField] private InputActionReference attackInput;
 
     [Header("Backstab Attributes")]
-    [Tooltip("Distancia minima para hacer un backStab")]
     [SerializeField] private float attackRange = 2f;
-
-    [Tooltip("Cuanto destras del enemigo tenemos que estar para poder hacer backstab (cuanto más bajo más ángulo)")]
     [SerializeField] private float backstabDotOffset = 0.65f;
-
-    [Tooltip("Cono de Vision del player en el que puede hacer el backstab (60º para quitarlo)")]
     [SerializeField] private float maxViewAngle = 60;
+    [SerializeField] private LayerMask enemyLayer;
 
-    private SphereCollider backstabCollider;
     private HashSet<ICanBackstab> enemies = new HashSet<ICanBackstab>();
     private ICanBackstab target;
-
-    void Awake()
-    {
-        backstabCollider = GetComponent<SphereCollider>();
-        backstabCollider.radius = attackRange; 
-    }
+    private Vector3 sphereOffset = new Vector3(0f, 1f, 0f);
 
     void Update()
     {
+        GatherEnemies();
         CheckIfCanBackstab();
         PerformBackstab();
     }
 
+    private void GatherEnemies()
+    {
+        Collider[] enemiesAround = Physics.OverlapSphere(transform.position + sphereOffset, attackRange, enemyLayer);
+        HashSet<ICanBackstab> newEnemies = new HashSet<ICanBackstab>();
+
+        foreach (Collider collider in enemiesAround)
+        {
+            if (collider.TryGetComponent(out ICanBackstab enemy))
+                newEnemies.Add(enemy);
+        }
+
+        foreach (ICanBackstab enemy in enemies)
+        {
+            if (!newEnemies.Contains(enemy))
+                enemy.SetWeakSpot(false);
+        }
+
+        enemies = newEnemies;
+    }
+
     private void CheckIfCanBackstab()
     {
-        if (enemies.Count == 0) return;
+        if (enemies.Count == 0)
+        {
+            target = null;
+            return;
+        }
 
         float bestDot = -1f;
         target = null;
@@ -44,22 +59,19 @@ public class BackStabController : MonoBehaviour
         foreach (ICanBackstab enemy in enemies)
         {
             Transform enemyTransform = enemy.GetTransform();
-            enemy.SetWeakSpot(false); 
+            enemy.SetWeakSpot(false);
 
-            // COMPROBAMOS QUE ESTAMOS DETRAS DEL ENEMIGO
             Vector3 dirFromEnemyToPlayer = (transform.position - enemyTransform.position).normalized;
             float dotBackstab = Vector3.Dot(enemyTransform.forward, dirFromEnemyToPlayer);
 
             if (dotBackstab > -backstabDotOffset) continue;
 
-            // ESTA DENTRO DE NUESTRO RANGO DE VISION
             Vector3 dirToEnemy = (enemyTransform.position - transform.position).normalized;
             float dotView = Vector3.Dot(transform.forward, dirToEnemy);
 
             float minDotView = Mathf.Cos(maxViewAngle * Mathf.Deg2Rad);
-            if (dotView < minDotView) continue; 
+            if (dotView < minDotView) continue;
 
-            // SI HAY MAS DE UN ENEMIGO, PONEMOS COMO TARGET AL ENEMIGO QUE ESTE MAS CENTRADO EN NUESTRA CAMARA
             if (dotView > bestDot)
             {
                 bestDot = dotView;
@@ -68,9 +80,7 @@ public class BackStabController : MonoBehaviour
         }
 
         if (target != null)
-        {
             target.SetWeakSpot(true);
-        }
     }
 
     private void PerformBackstab()
@@ -78,34 +88,16 @@ public class BackStabController : MonoBehaviour
         if (target != null && attackInput.action.triggered)
         {
             target.Backstab();
-            ClearTarget(target);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Enemy") && other.TryGetComponent(out ICanBackstab enemy))
-        {
-            enemies.Add(enemy);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Enemy") && other.TryGetComponent(out ICanBackstab enemy))
-        {
-            ClearTarget(enemy);
-        }
-    }
-
-    private void ClearTarget(ICanBackstab enemy)
-    {
-        if (enemy == target)
-        {
+            enemies.Remove(target);
+            target.SetWeakSpot(false);
             target = null;
-            enemy.SetWeakSpot(false);
         }
-        enemies.Remove(enemy);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + sphereOffset, attackRange);
     }
 
     private void OnEnable()
