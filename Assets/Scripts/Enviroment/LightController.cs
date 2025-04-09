b5228056772d65d8c64e4e294ed2f28724b2c8de
RubenMailloBaena <rubenmaillo2003@gmail.com>
2025-04-09 17:52:33 +0200

    lights and images

--- Assets/Scripts/Enviroment/LightController.cs
+++ Assets/Scripts/Enviroment/LightController.cs
@@ -1,14 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-    
     private PlayerController pController;
+    private bool playerInLight;
 
-    [SerializeField] private float playerOffset = 0.1f;
-   
     void Start()
     {
         pController = GameManager.GetInstance().GetPlayerController();
@@ -16,7 +15,8 @@ public class LightController : MonoBehaviour
 
     void Update()
     {
-        Vector3 playerPosition = pController.GetPlayerPosition();
+        LightPlayer();
+        /*Vector3 playerPosition = pController.GetPlayerPosition();
         playerPosition.y += playerOffset;
         Vector3 direction = (playerPosition - transform.position).normalized;
 
@@ -27,6 +27,42 @@ public class LightController : MonoBehaviour
             print(hit.collider.name);
             if (hit.collider.CompareTag("Player"))
                 pController.TakeDamage();
+        }*/
+    }
+
+    private void LightPlayer()
+    {
+        if (!playerInLight) return;
+        if(PerformRaycast(pController.GetPlayerPosition())) return;
+        if (PerformRaycast(pController.GetPlayerBodyPosition())) return;
+        PerformRaycast(pController.GetPlayerHeadPosition());
+    }
+
+    private bool PerformRaycast(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        
+        if (Physics.Raycast(transform.position, direction, out RaycastHit hit))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            if (hit.collider.CompareTag("Player"))
+            {
+                pController.TakeDamage();
+                return true;
+            }
         }
+        return false;
+    }
+
+private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            playerInLight = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            playerInLight = false;
     }
 }
