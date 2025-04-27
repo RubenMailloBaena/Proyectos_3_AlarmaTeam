0406d07d97984f567c8f6cd6a996c7587f2ef8d2
RubenMailloBaena <rubenmaillo2003@gmail.com>
2025-04-27 17:41:10 +0200

    Update Checkpoint.cs

--- Assets/Scripts/Logic/Checkpoint.cs
+++ Assets/Scripts/Logic/Checkpoint.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
